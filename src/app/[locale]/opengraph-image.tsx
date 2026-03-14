670958b31bfe32a4e96221e12e204df6f35b56f0
Marcelo B Diani <marcelodianib@gmail.com>
2026-03-13 22:37:32 -0300

    Update opengraph-image.tsx

--- src/app/[locale]/opengraph-image.tsx
+++ src/app/[locale]/opengraph-image.tsx
@@ -1,6 +1,6 @@
 import { ImageResponse } from "next/og";
 
-export const runtime = "edge";
+export const runtime = "nodejs";
 export const alt = "Marcelo B. Diani - Full Stack Developer";
 export const size = {
   width: 1200,
